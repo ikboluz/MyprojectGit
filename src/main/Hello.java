3e786020de2a4a5ea17814d5303765e85c6a1b40
Misha <antoshchuk.misha@gmail.com>
2018-03-10 12:54:59 -0500

    lol

--- src/main/Hello.java
+++ src/main/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("12345");
 		System.out.println("Hello");
+		System.out.println("");
+
+		// ikboluz push push
 	}
 	
 
