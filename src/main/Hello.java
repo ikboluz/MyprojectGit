9687220ba854501673eb0c9a136498cfd68f9599
ikbol009@gmail.com <ikbol009@gmail.com>
2018-03-10 12:56:37 -0500

    added

--- src/main/Hello.java
+++ src/main/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("12345");
 		System.out.println("Hello");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello Everybody");
+		}
 	}
 	
 
